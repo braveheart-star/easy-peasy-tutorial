792375170e41c1ede5e16457dced94d32b33db19
braveheart-star <mouse1996115@gmail.com>
2020-08-04 06:20:42 +0800

    connect the store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 
+import { StoreProvider } from "easy-peasy";
+import store from "./store";
+
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+
+ReactDOM.render(
+  <StoreProvider store={store}>
+    <App />
+  </StoreProvider>,
+  rootElement
+);
